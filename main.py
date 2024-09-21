f8036b4601895c86a166f03ebe2534f5f58a01ea
gab_ackles <166563651+gabriel5590@users.noreply.github.com>
2024-09-21 01:16:43 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,121 @@
+# By Gabriel Ackles 
+
+from termcolor import *
+import pyfiglet
+import os 
+import sys 
+resultado = pyfiglet.figlet_format("Soma da P . A")
+print(colored(resultado, 'light_magenta'))
+Pa = "(A1 + An) . n / 2"
+print(colored(Pa, 'light_magenta'))
+print(" ")
+
+numerodetermos = int(input("Digite o numero de termos dessa P.A (min 5) (max 11): "))
+if numerodetermos == 5:
+	a1 = int(input("Digite o a1: "))
+	a2 = int(input("Digite o a2: "))
+	a3 = int(input("Digite o a3: " ))
+	a4 = int(input("Digite o a4: "))
+	a5 = int(input("Digite o a5: "))
+	soma = a1 + a5
+	multiplicar = soma * 5
+	dividir = multiplicar / 2 
+	resultado = dividir 
+	print(resultado)
+
+if numerodetermos == 6:
+	a1 = int(input("Digite o a1: "))
+	a2 = int(input("Digite o a2: "))
+	a3 = int(input("Digite o a3: " ))
+	a4 = int(input("Digite o a4: "))
+	a5 = int(input("Digite o a5: "))
+	a6 = int(input("Digite o a6: "))
+	soma = a1 + a6
+	multiplicar = soma * 6
+	dividir = multiplicar / 2 
+	resultado = dividir 
+	print(resultado)
+
+if numerodetermos == 7:
+	a1 = int(input("Digite o a1: "))
+	a2 = int(input("Digite o a2: "))
+	a3 = int(input("Digite o a3: " ))
+	a4 = int(input("Digite o a4: "))
+	a5 = int(input("Digite o a5: "))
+	a6 = int(input("Digite o a6: "))
+	a7 = int(input("Digite o a7: "))
+	
+	soma = a1 + a7
+	multiplicar = soma * 7
+	dividir = multiplicar / 2 
+	resultado = dividir 
+	print(resultado)
+
+if numerodetermos == 8:
+	a1 = int(input("Digite o a1: "))
+	a2 = int(input("Digite o a2: "))
+	a3 = int(input("Digite o a3: " ))
+	a4 = int(input("Digite o a4: "))
+	a5 = int(input("Digite o a5: "))
+	a6 = int(input("Digite o a6: "))
+	a7 = int(input("Digite o a7: "))
+	a8 = int(input("Digite o a8: "))
+	
+	
+	soma = a1 + a8
+	multiplicar = soma * 8
+	dividir = multiplicar / 2 
+	resultado = dividir 
+	print(resultado)
+
+if numerodetermos == 9:
+	a1 = int(input("Digite o a1: "))
+	a2 = int(input("Digite o a2: "))
+	a3 = int(input("Digite o a3: " ))
+	a4 = int(input("Digite o a4: "))
+	a5 = int(input("Digite o a5: "))
+	a6 = int(input("Digite o a6: "))
+	a7 = int(input("Digite o a7: "))
+	a8 = int(input("Digite o a8: "))
+	a9 = int(input("Digite o a9: "))
+	
+	soma = a1 + a9
+	multiplicar = soma * 9
+	dividir = multiplicar / 2 
+	resultado = dividir 
+	print(resultado)
+
+if numerodetermos == 10:
+	a1 = int(input("Digite o a1: "))
+	a2 = int(input("Digite o a2: "))
+	a3 = int(input("Digite o a3: " ))
+	a4 = int(input("Digite o a4: "))
+	a5 = int(input("Digite o a5: "))
+	a6 = int(input("Digite o a6: "))
+	a7 = int(input("Digite o a7: "))
+	a8 = int(input("Digite o a8: "))
+	a9 = int(input("Digite o a9: "))
+	a10 = int(input("Digite o a10: "))
+	soma = a1 + a10
+	multiplicar = soma * 10
+	dividir = multiplicar / 2 
+	resultado = dividir 
+	print(resultado)
+
+if numerodetermos == 11:
+	a1 = int(input("Digite o a1: "))
+	a2 = int(input("Digite o a2: "))
+	a3 = int(input("Digite o a3: " ))
+	a4 = int(input("Digite o a4: "))
+	a5 = int(input("Digite o a5: "))
+	a6 = int(input("Digite o a6: "))
+	a7= int(input("Digite o a7: "))
+	a8 = int(input("Digite o a8: "))
+	a9 = int(input("Digite o a9: "))
+	a10 = int(input("Digite o a10: "))
+	a11 = int(input("Digite o a11: "))
+	soma = a1 + a11
+	multiplicar = soma * 11
+	dividir = multiplicar / 2 
+	resultado = dividir 
+	print(resultado)
